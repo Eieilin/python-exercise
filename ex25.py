5518d45cd11af1af972f78b342a9e08d7825b76d
Eieilin <Eieilin2123@gmail.com>
2018-06-12 19:21:32 +0630

    Even more Practice

--- ex25.py
+++ ex25.py
@@ -4,7 +4,7 @@ def break_words(stuff):
     return words
 
 def sort_words(words):
-    """Sorts the words."""
+    """Sort the words."""
     return sorted(words)
 
 def print_first_word(words):
@@ -14,16 +14,16 @@ def print_first_word(words):
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1)
+    word = word.pop(-1)
     print(word)
 
 def sort_sentence(sentence):
-    """Takes in a full sentence and returns the sorted words
+    """Takes in a full sentence and returns the sorted words"""
     words = break_words(sentence)
     return sort_words(words)
 
 def print_first_and_last(sentence):
-    """Prints the first an last words of the sentence."""
+    """Prints the first and last words of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
     print_last_word(words)
