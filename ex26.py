87c9a0c5a1c2d747633f14ac062bc39ac6ed0079
Eieilin <Eieilin2123@gmail.com>
2018-06-12 21:09:39 +0630

    Memorizing logic

--- ex26.py
+++ ex26.py
@@ -7,14 +7,15 @@ weight = input()
 
 print(f"So , you're {age} old, {height} tall and {weight} heavy.")
 
-script, filename =argv
+from sys import argv
+script, filename = argv
 
 txt = open(filename)
 
 print("Here's your file {filename}:")
 print(txt.read())
 
-print("Type the filename again:")f
+print("Type the filename again:")
 file_again = input()
 
 txt_again = open(file_again)
@@ -41,14 +42,14 @@ print("--------------------")
 five = 10 - 2 + 3 - 6
 print(f"This should be five: {five}")
 
-def secret_formula(started)
+def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars + 100
+    crates = jars / 100
     return jelly_beans, jars, crates
 
 start_point = 10000
-beans, jars = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point)
 
 print("With a starting point of: {}".format(start_point))
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
@@ -56,7 +57,7 @@ print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 print("We'd have{} beans,  {} jars, and {} crates.".format(*formula))
 
 people =20
@@ -82,5 +83,5 @@ if people >= dogs:
 if people <= dogs:
     print("People are less than or equal to dogs.")
 
-if people = dogs:
+if people == dogs:
     print("People are equal dogs.")
