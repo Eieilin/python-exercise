c94f922bd25546ce90bb57e41b9eb18a8cf158c7
Eieilin <Eieilin2123@gmail.com>
2018-06-13 23:07:24 +0630

    Loop and list

--- ex31.py
+++ ex31.py
@@ -9,15 +9,15 @@ if door == "1":
     print("1. Take the cake.")
     print("2. Scream at the bear.")
 
-bear = input("> ")
+    bear = input("> ")
 
-if bear = "1":
-    print("The bear eats your face off. Good job!")
-elif bear == "2":
-    print("The bear eats your legs off. Good job!")
-else:
-    print(f"Well, doing {bear} is probably better.")
-    print("Bear runs away.")
+    if bear == "1":
+       print("The bear eats your face off. Good job!")
+    elif bear == "2":
+       print("The bear eats your legs off. Good job!")
+    else:
+       print(f"Well, doing {bear} is probably better.")
+       print("Bear runs away.")
 
 elif door == "2":
     print("You stare into the endless abyss at Cthulhu's res")
@@ -25,13 +25,13 @@ elif door == "2":
     print("2. Yellow jacket clothespins.")
     print("3. Understanding revolvers yelling melodies.")
 
-insanity = input("> ")
+    insanity = input("> ")
 
-  if insanity == "1" or insanity == "2":
-    print("Your body surbives powered by a mind of jello ")
-    print("Good job!")
-  else:
-    print("The insanity rots your eyes into a pool of mu")
-    print("Good job!")
+    if insanity == "1" or insanity == "2":
+        print("Your body surbives powered by a mind of jello ")
+        print("Good job!")
+    else:
+        print("The insanity rots your eyes into a pool of mu")
+        print("Good job!")
 else:
     print("You stumble around and fall on a knife and deie.")
