5353575b9e149aa0a85a9bb2519be62638d22405
Eieilin <Eieilin2123@gmail.com>
2018-06-13 23:31:59 +0630

    Accessing Elements of lists

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'rabbit', 'peacock', 'kangaroo', 'whale','shark']
+
+1.The animal at 1. = bear
+2.The third(3rd) animal. = rabbit
+3.The first animal. =bear
+4.The  animal at 3 = peacock
+5.The fifth animal = kangaroo
+6.The animal at 2 = rabbit
+7.The sixth animal = whale
+8.The animal at 4 = kangaroo
