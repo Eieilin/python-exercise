5449862ee122cbe1aa29f8e7d6e6b54213b769d1
Eieilin <Eieilin2123@gmail.com>
2018-06-17 20:45:17 +0630

    Is-A, Has-A, Objects, and Classes

---
+++ ex40a.py
@@ -0,0 +1,16 @@
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple'])
+def apple():
+    print("I AM APPLES!")
+
+
+def apple():
+    print("I am Apple")
+tangerine = "Living reflection of a dream"
+
+mystuff.apple()
+print(mystuff.tangerine)
+mystuff['apple']
+mystuff.apple()
+mystuff.tangerine
+
